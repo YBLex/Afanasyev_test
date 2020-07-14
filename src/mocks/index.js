c8511da7a7f29df68aaf500d4124b6586315028d
YB <alex56599@yandex.ru>
2020-07-15 00:47:06 +0300

    Add mocks

---
+++ src/mocks/index.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    name: 'Alex',
+    tel: '8 (999) 999-99-99',
+    children: [
+      {
+        id: 5,
+        name: 'Misha',
+        tel: '8 (888) 888-88-88',
+        children: [
+          {
+            id: 5,
+            name: 'Roma',
+            tel: '8 (888) 888-88-88',
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Roma',
+        tel: '8 (888) 888-88-88',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Masha',
+    tel: '8 (999) 999-99-99',
+    children: [],
+  },
+  {
+    id: 3,
+    name: 'Andrew',
+    tel: '8 (999) 999-99-99',
+    children: [],
+  },
+  {
+    id: 4,
+    name: 'Nastya',
+    tel: '8 (999) 999-99-99',
+    children: [],
+  },
+  {
+    id: 1,
+    name: 'Sveta',
+    tel: '8 (999) 999-99-99',
+    children: [],
+  },
+];
